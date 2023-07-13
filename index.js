fc28ee7ed6b4acf78a072d640975c5ee8ba6fff8
EmilKhamatnurov <emil.khamatnurov@gmail.com>
2023-07-13 19:47:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const suggestionOutputNode = document.querySelector('#suggestionOutput');
 const createSuggestionBtnNode = document.querySelector('#createSuggestionBtn');
 
 createSuggestionBtnNode.addEventListener('click', createSuggestion)
-function createSuggestion() {
+await function createSuggestion() {
 	fetch('http://www.boredapi.com/api/activity/')
 	.then(request => request.json())
 	.then(suggestion => {suggestionOutputNode.innerText = suggestion.activity});
