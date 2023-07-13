e970c1f527080318f5d5707a0887a575df49c8e1
EmilKhamatnurov <emil.khamatnurov@gmail.com>
2023-07-13 19:44:50 +0500

    Исправлена проблема с приемом данных

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const createSuggestionBtnNode = document.querySelector('#createSuggestionBtn');
 createSuggestionBtnNode.addEventListener('click', createSuggestion)
 function createSuggestion() {
 	fetch('http://www.boredapi.com/api/activity/')
+	.then(request => request.json())
 	.then(suggestion => {suggestionOutputNode.innerText = suggestion.activity});
 }
