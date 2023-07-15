3086b6aa54a0dbfdb3e80e65eeb0ba61f8878986
EmilKhamatnurov <emil.khamatnurov@gmail.com>
2023-07-15 22:03:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const suggestionOutputNode = document.querySelector('#suggestionOutput');
 const createSuggestionBtnNode = document.querySelector('#createSuggestionBtn');
 
-createSuggestionBtnNode.addEventListener('click', function () {
-	fetch('http://www.boredapi.com/api/activity/')
-	.then(request => request.json())
-	.then(suggestion => {suggestionOutputNode.innerText = suggestion.activity});
-});
-// await function createSuggestion() {
+createSuggestionBtnNode.addEventListener('click', createSuggestion);
+function createSuggestion() {
+	fetch('https://www.boredapi.com/api/activity/')
 	
-// }
+		.then(request => request.json())
+		.then(suggestion => {suggestionOutputNode.innerText = suggestion.activity});
+}
+// У меня нет понимания, для чего используется функция await 
+
